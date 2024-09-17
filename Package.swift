226853f3fa11cb46ef7a29c9f8af0a432362dc6d
lahari <gplahari@gmail.com>
2024-09-17 15:49:21 +0200

    Update packages

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/Kitura/BlueSocket.git", from: "2.0.2"),
         .package(url: "https://github.com/Kitura/BlueSSLService.git", from: "2.0.1"),
         .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.1"),
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0"),
         ],
     targets: [
         .target(
